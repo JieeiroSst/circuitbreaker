f1dadf70d112cc4cbe1509efa875ca33bede9b16
Alain Liu <luumanhquan.91@gmail.com>
2024-07-29 23:24:39 +0700

    add circuit breaker

--- go.mod
+++ go.mod
@@ -4,4 +4,6 @@ go 1.21
 
 toolchain go1.22.2
 
-require github.com/sony/gobreaker/v2 v2.0.0
+require github.com/sony/gobreaker v1.0.0
+
+require github.com/stretchr/testify v1.8.4 // indirect
--- main.go
+++ main.go
@@ -1,55 +1,134 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"errors"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
-	"github.com/sony/gobreaker/v2"
+	"github.com/sony/gobreaker"
 )
 
-var cb *gobreaker.CircuitBreaker[[]byte]
+//server
 
-func init() {
-	var st gobreaker.Settings
-	st.Name = "HTTP GET"
-	st.ReadyToTrip = func(counts gobreaker.Counts) bool {
-		failureRatio := float64(counts.TotalFailures) / float64(counts.Requests)
-		return counts.Requests >= 3 && failureRatio >= 0.6
-	}
-
-	cb = gobreaker.NewCircuitBreaker[[]byte](st)
+type ExampleServer struct {
+	addr      string
+	logger    *log.Logger
+	isEnabled bool
 }
 
-// Get wraps http.Get in CircuitBreaker.
-func Get(url string) ([]byte, error) {
-	body, err := cb.Execute(func() ([]byte, error) {
-		resp, err := http.Get(url)
-		if err != nil {
-			return nil, err
-		}
+func NewExampleServer(addr string) *ExampleServer {
+	return &ExampleServer{
+		addr:      addr,
+		logger:    log.New(os.Stdout, "Server\t", log.LstdFlags),
+		isEnabled: true,
+	}
+}
 
-		defer resp.Body.Close()
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			return nil, err
+func (s *ExampleServer) ListenAndServe() error {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if s.isEnabled {
+			s.logger.Println("responded with OK")
+			w.WriteHeader(http.StatusOK)
+		} else {
+			s.logger.Println("responded with Error")
+			w.WriteHeader(http.StatusInternalServerError)
 		}
-
-		return body, nil
 	})
+
+	http.HandleFunc("/toggle", func(w http.ResponseWriter, r *http.Request) {
+		s.isEnabled = !s.isEnabled
+		s.logger.Println("toggled. Is enabled:", s.isEnabled)
+		w.WriteHeader(http.StatusOK)
+	})
+
+	return http.ListenAndServe(s.addr, nil)
+}
+
+// Client
+type NotificationClient interface {
+	Send() error
+}
+
+type SmsClient struct {
+	baseUrl string
+}
+
+func NewSmsClient(baseUrl string) *SmsClient {
+	return &SmsClient{
+		baseUrl: baseUrl,
+	}
+}
+
+func (s *SmsClient) Send() error {
+	url := s.baseUrl + "/"
+	resp, err := http.Get(url)
 	if err != nil {
-		return nil, err
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return errors.New("bad response")
 	}
 
-	return body, nil
+	return nil
+}
+
+// circuit_breaker
+type ClientCircuitBreakerProxy struct {
+	client NotificationClient
+	logger *log.Logger
+	gb     *gobreaker.CircuitBreaker
+}
+
+func shouldBeSwitchedToOpen(counts gobreaker.Counts) bool {
+	failureRatio := float64(counts.TotalFailures) / float64(counts.Requests)
+	return counts.Requests >= 3 && failureRatio >= 0.6
+}
+
+func NewClientCircuitBreakerProxy(client NotificationClient) *ClientCircuitBreakerProxy {
+	logger := log.New(os.Stdout, "CB\t", log.LstdFlags)
+
+	cfg := gobreaker.Settings{
+		Interval:    5 * time.Second,
+		Timeout:     7 * time.Second,
+		ReadyToTrip: shouldBeSwitchedToOpen,
+		OnStateChange: func(_ string, from gobreaker.State, to gobreaker.State) {
+			logger.Println("state changed from", from.String(), "to", to.String())
+		},
+	}
+
+	return &ClientCircuitBreakerProxy{
+		client: client,
+		logger: logger,
+		gb:     gobreaker.NewCircuitBreaker(cfg),
+	}
+}
+
+func (c *ClientCircuitBreakerProxy) Send() error {
+	_, err := c.gb.Execute(func() (interface{}, error) {
+		err := c.client.Send()
+		return nil, err
+	})
+	return err
 }
 
 func main() {
-	body, err := Get("http://www.google.com/robots.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+	logger := log.New(os.Stdout, "Main\t", log.LstdFlags)
+	server := NewExampleServer(":8080")
 
-	fmt.Println(string(body))
+	go func() {
+		_ = server.ListenAndServe()
+	}()
+
+	client := NewSmsClient("http://127.0.0.1:8080")
+
+	for {
+		err := client.Send()
+		time.Sleep(1 * time.Second)
+		if err != nil {
+			logger.Println("caught an error", err)
+		}
+	}
 }
